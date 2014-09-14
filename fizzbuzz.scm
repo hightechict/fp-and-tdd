64e77e9bd77a8597ed37ba5dd2540772a5929ddf
Bas Bossink <bas.bossink@gmail.com>
2014-09-14 22:01:56 +0200

    Convert fizzbuzz procedure to use simple define syntax.

--- fizzbuzz.scm
+++ fizzbuzz.scm
@@ -1,24 +1,25 @@
-(define fizzbuzz (lambda (x) 
-  (cond ((= 0 (modulo x 15)) "fizzbuzz")
-        ((= 0 (modulo x 3)) "fizz")
-        ((= 0 (modulo x 5)) "buzz")
-        (else x))))
+(define fizzbuzz 
+  (lambda (x) 
+    (cond ((= 0 (modulo x 15)) "fizzbuzz")
+          ((= 0 (modulo x 3)) "fizz")
+          ((= 0 (modulo x 5)) "buzz")
+          (else x))))
 
 (let (
-  (assert-fizzbuzz 
-    (lambda (expected parameter)
-      (let ((actual (fizzbuzz parameter)))
-      (test (format "(fizzbuzz ~a) = ~a expected ~a" parameter actual expected) 
-            expected (fizzbuzz parameter)))))
-  (test-cases 
-    (list 
-      '(1 1) 
-      '(2 2)
-      '(31 31)
-      '("fizz" 3)
-      '("buzz" 5 )
-      '("buzz" 10)
-      '("fizz" 6)
-      '("fizzbuzz" 15)
-      '("fizzbuzz" 30))))
+    (assert-fizzbuzz 
+      (lambda (expected parameter)
+        (let ((actual (fizzbuzz parameter)))
+        (test (format "(fizzbuzz ~a) = ~a expected ~a" parameter actual expected) 
+              expected (fizzbuzz parameter)))))
+    (test-cases 
+      (list 
+        '(1 1) 
+        '(2 2)
+        '(31 31)
+        '("fizz" 3)
+        '("buzz" 5 )
+        '("buzz" 10)
+        '("fizz" 6)
+        '("fizzbuzz" 15)
+        '("fizzbuzz" 30))))
   (map (lambda (x) (apply assert-fizzbuzz x)) test-cases))
