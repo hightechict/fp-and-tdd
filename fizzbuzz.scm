49c70551a4601e29914f5ffe6af70dddc450741c
Bas Bossink <bas.bossink@gmail.com>
2014-09-14 11:57:19 +0200

    Rewrite fizzbuzz and test to be simpler and more readable.
    - use define only to define variables
    - reduce the scope of assert-fizzbuzz and test-cases
    - use apply to simply map application

--- fizzbuzz.scm
+++ fizzbuzz.scm
@@ -1,24 +1,24 @@
-(define (fizzbuzz x) 
+(define fizzbuzz (lambda (x) 
   (cond ((= 0 (modulo x 15)) "fizzbuzz")
         ((= 0 (modulo x 3)) "fizz")
         ((= 0 (modulo x 5)) "buzz")
-        (else x)))
+        (else x))))
 
-(define (assert-fizzbuzz expected parameter)
-  (let ((actual (fizzbuzz parameter)))
-  (test (format "(fizzbuzz ~a) = ~a expected ~a" parameter actual expected) 
-        expected (fizzbuzz parameter))))
-
-(define test-cases 
-  (list 
-    '(1 . 1) 
-    '(2 . 2)
-    '(31 . 31)
-    '("fizz" . 3)
-    '("buzz" . 5 )
-    '("buzz" . 10)
-    '("fizz" . 6)
-    '("fizzbuzz" . 15)
-    '("fizzbuzz" . 30)))
-
-(map (lambda (x) (assert-fizzbuzz (car x) (cdr x))) test-cases)
+(let (
+  (assert-fizzbuzz 
+    (lambda (expected parameter)
+      (let ((actual (fizzbuzz parameter)))
+      (test (format "(fizzbuzz ~a) = ~a expected ~a" parameter actual expected) 
+            expected (fizzbuzz parameter)))))
+  (test-cases 
+    (list 
+      '(1 1) 
+      '(2 2)
+      '(31 31)
+      '("fizz" 3)
+      '("buzz" 5 )
+      '("buzz" 10)
+      '("fizz" 6)
+      '("fizzbuzz" 15)
+      '("fizzbuzz" 30))))
+  (map (lambda (x) (apply assert-fizzbuzz x)) test-cases))
