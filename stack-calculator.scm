ee3b336666a16e89ad9667d81fd2c83d66e67112
Bas Bossink <bas.bossink@gmail.com>
2014-10-23 11:25:25 +0200

    Add support for square root.

--- stack-calculator.scm
+++ stack-calculator.scm
@@ -1,16 +1,20 @@
 (load "check.scm")
 
-(define operator? (lambda (exp)
-  (let ((token (car exp)))
+(define unary-operator? (lambda (expression)
+  (eqv? 'sqrt (car expression))))
+
+(define operator? (lambda (expression)
+  (let ((token (car expression)))
     (or (eqv? '+ token)
         (eqv? '- token)
         (eqv? '* token)
         (eqv? '^ token)
+        (unary-operator? expression)
         (eqv? '/ token)))))
 
 (define operator 
-  (lambda (exp) 
-    (let ((oper (car exp)))
+  (lambda (expression) 
+    (let ((oper (car expression)))
       (cond ((eqv? '^ oper) 'expt)
             (else oper)))))
 
@@ -20,6 +24,11 @@
     (if (null? expression)
         "there is nothing to calculate!"
           (cond ((null? exp) (car stack))
+                ((unary-operator? exp)
+                 (let* ((first-operand (car stack))
+                        (result (eval (list (operator exp)
+                                            first-operand))))
+                   (calculate (cdr exp) (cons result (cdr stack)))))
                 ((operator? exp)
                  (let* ((first-operand (cadr stack))
                         (second-operand (car stack))
@@ -29,22 +38,28 @@
                 (else (calculate (cdr exp) (cons (car exp) stack)))))))
   (calculate expression '()))
 
-(check (rpn '(2 3 +)) => 5)
-(check (rpn '(2 3 *)) => 6)
-(check (rpn '(6 2 /)) => 3)
-(check (rpn '(3 2 -)) => 1)
-(check (rpn '(2 3 ^ )) => 8)
-
 (check (operator? '(+)) => #t)
 (check (operator? '(-)) => #t)
 (check (operator? '(*)) => #t)
 (check (operator? '(/)) => #t)
 (check (operator? '(^)) => #t)
+(check (operator? '(sqrt)) => #t)
+
+(check (unary-operator? '(sqrt)) => #t)
+(check (unary-operator? '(+)) => #f)
 
 (check (operator '(+)) => '+)
 (check (operator '(-)) => '-)
 (check (operator '(*)) => '*)
 (check (operator '(/)) => '/)
 (check (operator '(^)) => 'expt)
+(check (operator '(sqrt)) => 'sqrt)
+
+(check (rpn '(2 3 +)) => 5)
+(check (rpn '(2 3 *)) => 6)
+(check (rpn '(6 2 /)) => 3)
+(check (rpn '(3 2 -)) => 1)
+(check (rpn '(2 3 ^ )) => 8)
+(check (rpn '(4 sqrt)) => 2.0)
 
 (check-report)
