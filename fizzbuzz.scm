dfbd44097dcab13a2ef5df1f337e39e24c53e608
Bas Bossink <bas.bossink@gmail.com>
2014-10-23 11:02:37 +0200

    Switch to using srfi-78.

--- fizzbuzz.scm
+++ fizzbuzz.scm
@@ -1,25 +1,25 @@
+(load "check.scm")
+
 (define fizzbuzz 
   (lambda (x) 
-    (cond ((= 0 (modulo x 15)) "fizzbuzz")
-          ((= 0 (modulo x 3)) "fizz")
-          ((= 0 (modulo x 5)) "buzz")
+    (cond ((= 0 (mod x 15)) "fizzbuzz")
+          ((= 0 (mod x 3)) "fizz")
+          ((= 0 (mod x 5)) "buzz")
           (else x))))
-
 (let (
-    (assert-fizzbuzz 
-      (lambda (expected parameter)
-        (let ((actual (fizzbuzz parameter)))
-        (test (format "(fizzbuzz ~a) = ~a expected ~a" parameter actual expected) 
-              expected (fizzbuzz parameter)))))
-    (test-cases 
-      (list 
-        '(1 1) 
-        '(2 2)
-        '(31 31)
-        '("fizz" 3)
-        '("buzz" 5 )
-        '("buzz" 10)
-        '("fizz" 6)
-        '("fizzbuzz" 15)
-        '("fizzbuzz" 30))))
-  (map (lambda (x) (apply assert-fizzbuzz x)) test-cases))
+      (check-fizzbuzz 
+        (lambda (check-value expected)
+          (check (fizzbuzz check-value) => expected)))
+      (test-cases (list
+                '(1 1)
+                '(2 2)
+                '(3 "fizz")
+                '(5 "buzz")
+                '(9 "fizz")
+                '(10 "buzz")
+                '(15 "fizzbuzz")
+                '(30 "fizzbuzz")
+                '(37 37))))
+  (map (lambda (x) (apply check-fizzbuzz x)) test-cases))
+
+(check-report)
