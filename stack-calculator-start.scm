778da798b7076f4b6d1ee609e6878fc999fde2b0
Bas Bossink <bas.bossink@gmail.com>
2014-11-13 01:22:45 +0100

    Update after feedback during first try out.

--- stack-calculator-start.scm
+++ stack-calculator-start.scm
@@ -1,34 +1,35 @@
 (load "~/.bin/check.scm")
 (check-set-mode! 'report-failed)
 
+(define operator
+  (lambda (expression) 
+    (car expression)))
+
+(define operator? 
+  (lambda (expression)
+    (define token (car expression))
+    (or (eq? '+ token)
+        (eq? '- token)
+        (eq? '* token)
+        (eq? '/ token))))
+
+(define calculate (lambda (expr stack)
+    (cond ((null? expr) (car stack))
+          ((operator? expr)
+            (define first-operand (cadr stack))
+            (define second-operand (car stack))
+            (define result (eval (list 
+                                   (operator expr) 
+                                   first-operand 
+                                   second-operand)))
+            (calculate (cdr expr) (cons result (cddr stack))))
+          (else (calculate (cdr expr) (cons (car expr) stack))))))
+
 (define (rpn expression)
-  (letrec (
-    (operator? 
-      (lambda (expression)
-        (let ((token (car expression)))
-          (or (eq? '+ token)
-              (eq? '- token)
-              (eq? '* token)
-              (eq? '/ token)))))
-    (operator 
-      (lambda (expression) 
-        (car expression)))
-    (calculate 
-      (lambda (expr stack)
-        (cond ((null? expr) (car stack))
-              ((operator? expr)
-               (let* ((first-operand (cadr stack))
-                      (second-operand (car stack))
-                      (result (eval (list 
-                                      (operator expr) 
-                                      first-operand 
-                                      second-operand))))
-                 (calculate (cdr expr) (cons result (cddr stack)))))
-              (else (calculate (cdr expr) (cons (car expr) stack)))))))
     (if (null? expression)
       "there is nothing to calculate!"
-      (calculate expression '()))))
+      (calculate expression '())))
 
-(check (rpn '()) => )
+(check (rpn '(12 12 *)) => 144)
 
 (check-report)
